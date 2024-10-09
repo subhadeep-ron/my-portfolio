8e3e42c59b558570210e1eac982bba13a4d33fb1
subhadeep-ron <subhadeep90074@gmail.com>
2024-10-09 20:40:37 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Your message has been sent!');
+});
+
+document.getElementById("mark1").textContent = "xxxx";
+document.getElementById("mark2").textContent = "xxxx";
+document.getElementById("mark3").textContent = "xxxx";
+document.getElementById("mark4").textContent = "xxxx";
