94f135b4dac99704069ce9dc3f44862c03767694
subhadeep-ron <subhadeep90074@gmail.com>
2025-04-14 20:08:15 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,19 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     alert('Your message has been sent!');
 });
 
-document.getElementById("mark1").textContent = "xxxx";
-document.getElementById("mark2").textContent = "xxxx";
-document.getElementById("mark3").textContent = "xxxx";
-document.getElementById("mark4").textContent = "xxxx";
+var bt_stat = 0;
+document.getElementById('cv_show').onclick = function()
+{
+    if(bt_stat == 0)
+    {
+        document.getElementById('my-cv').style = "display: block";
+        document.getElementById('cv_show').innerHTML = "Click Here to Hide The Resume";
+        bt_stat = 1;
+    }
+    else
+    {
+        document.getElementById('my-cv').style = "display: none";
+        document.getElementById('cv_show').innerHTML = "Click Here to view The Resume";
+        bt_stat = 0;
+    }
+};
