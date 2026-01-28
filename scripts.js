4707a0772240945ff8784f63df79749cb20bcfce
subhadeep-ron <subhadeep90074@gmail.com>
2026-01-28 09:55:53 +0530

    Update scripts.js
    modified the interactiveness and cv part

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Your message has been sent!');
+/*
+document.getElementById("cv_show").addEventListener("click", function() {
+    const cv = document.getElementById("my-cv");
+    cv.style.display = (cv.style.display === "none" || cv.style.display === "") ? "block" : "none";
 });
 
 var bt_stat = 0;
@@ -18,4 +19,31 @@ document.getElementById('cv_show').onclick = function()
         document.getElementById('cv_show').innerHTML = "Click Here to View";
         bt_stat = 0;
     }
-};
+};*/
+
+var bt_stat = 0;
+document.getElementById("cv_show").addEventListener("click", function() {
+    const cv = document.getElementById("cv-container");
+    cv.style.display = (cv.style.display === "none" || cv.style.display === "") 
+        ? "block" 
+        : "none";
+    
+    if(bt_stat == 0)
+    {
+        //document.getElementById('my-cv').style = "display: block";
+        document.getElementById('cv_show').innerHTML = "Click Again to Hide";
+        bt_stat = 1;
+    }
+    else
+    {
+        //document.getElementById('my-cv').style = "display: none";
+        document.getElementById('cv_show').innerHTML = "Click Here to View";
+        bt_stat = 0;
+    }
+});
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
